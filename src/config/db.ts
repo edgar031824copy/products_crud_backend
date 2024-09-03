7c472ca6dccb70f77d7fbae1c73b468fcd7ff88f
Edgar <edgar@Edgars-MacBook-Air.local>
2024-09-03 18:04:51 -0500

    add build scipt

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 dotenv.config();
 const db = new Sequelize(`${process.env.DATABASE_URL}`, {
-  models: [path.join(__dirname, '../models/**/*.ts')],
+  models: [path.join(__dirname, '../models/**/*')],
   logging: false,
 });
 export default db;
